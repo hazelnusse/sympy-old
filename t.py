becfee1c96013784518f4b991e95bc365e9034c8
Luke Peterson <hazelnusse@gmail.com>
2009-06-26 09:26:29 -0700

    trying to removing conflicting part of t.py

--- t.py
+++ t.py
@@ -133,35 +133,6 @@ tan = Tan
 cot = Cot
 
 
-"""
-a, b = symbols('a b')# map(Wild, 'ab')
-#'TR1': ((sec(a), S(1)/cos(a)), (csc(a), S(1)/sin(a))),
-
-rule_dict = {'TR2': ((tan(a), sin(a)/cos(a)), (cot(a), cos(a)/sin(a))),
-                  'TR5': ((sin(a)**2, 1 - cos(a)**2)),
-                  'TR6': ((cos(a)**2, 1 - sin(a)**2)),
-                  'TR7': ((cos(a)**2, (1 + cos(2*a))/2)),
-                  'TR8': ((sin(a)*cos(b), (sin(a + b) + sin(a - b))/2))}
-#                          (cos(a)*sin(b), (sin(a + b) - sin(a - b))/2),
-#                          (cos(a)*cos(b), (cos(a + b) + cos(a - b))/2),
-#                          (sin(a)*sin(b), -(cos(a + b) - cos(a - b))/2))}
-#                  'TR9': ((sin(a) + sin(b), 2*sin((a + b)/2)*cos((a - b)/2)),
-#                          (sin(a) - sin(b), 2*cos((a + b)/2)*sin((a - b)/2)),
-#                          (cos(a) + cos(b), 2*cos((a + b)/2)*cos((a - b)/2)),
-#                          (cos(a) - cos(b), -2*sin((a + b)/2)*sin((a - b)/2))),
-#                  'TR10': ((sin(a + b), sin(a)*cos(b) + cos(a)*sin(b)),
-##                           (sin(a - b), sin(a)*cos(b) - cos(a)*sin(b)),
-##                           (cos(a + b), cos(a)*cos(b) - sin(a)*sin(b)),
-#                           (cos(a - b), cos(a)*cos(b) + sin(a)*sin(b))),
-#                  'TR11': ((sin(2*a), 2*sin(a)*cos(a)),
-#                           (cos(2*a), cos(a)**2 - sin(a)**2)),
-#                  'TR12': ((tan(a + b), (tan(a) + tan(b))/(1 - tan(a)*tan(b))),
-##                           (tan(a - b), (tan(a) - tan(b))/(1 +
-#                               tan(a)*tan(b)))),
-#                  'TR13': ((tan(a)*tan(b), 1 - (tan(a) + tan(b))*cot(a + b)),
-#                           (cot(a)*cot(b), 1 + (cot(a) + cot(b))*cot(a + b)))}
-
-"""
 var("x n N y")
 ex1 = 1 - 1/4*sin(2*x)**2 - sin(y)**2 - cos(x)**4
 ex2 = cos(pi/9)*cos(2*pi/9)*cos(3*pi/9)*cos(4*pi/9)
